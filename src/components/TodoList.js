9aff8b797feb739593d691f36b5555b734f7ad0d
abdimak1 <abdimak602@gmail.com>
2024-01-06 18:27:49 -0800

    first commit

---
+++ src/components/TodoList.js
@@ -0,0 +1,46 @@
+// TodoList.js
+import React, { useState } from 'react';
+import './TodoList.css'; 
+
+const TodoList = () => {
+  const [todos, setTodos] = useState([]);
+  const [task, setTask] = useState('');
+
+  const addTodo = () => {
+    if (task.trim() !== '') {
+      setTodos([...todos, task]);
+      setTask('');
+    }
+  };
+
+  const removeTodo = (index) => {
+    const updatedTodos = [...todos];
+    updatedTodos.splice(index, 1);
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div>
+      <h2>Todo List</h2>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => removeTodo(index)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+      <div>
+        <input
+          type="text"
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+          placeholder="Add a new task"
+        />
+        <button onClick={addTodo}>Add Task</button>
+      </div>
+    </div>
+  );
+};
+
+export default TodoList;
